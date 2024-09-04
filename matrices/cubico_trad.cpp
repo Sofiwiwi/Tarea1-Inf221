2fc78ad634772b35b29de3eff1663b5d771403b9
Sofiwiwi <sofia.ramirezc@usm.cl>
2024-09-04 09:39:33 -0400

    cálculo de matrices nxm y mxp, cálculo de matrices nxn

--- matrices/cubico_trad.cpp
+++ matrices/cubico_trad.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 
-vector<vector<int> cubico_trad(vector<vector<int>> &matriz_A, vector<vector<int>> &matriz_B, vector<vector<int>>){
+vector<vector<int>> cubico_trad(vector<vector<int>> &matriz_A, vector<vector<int>> &matriz_B){
     int n = matriz_A.size();
     int m = matriz_A[0].size();
     int p = matriz_B[0].size();
     vector<vector<int>> matriz_C(n, vector<int>(p));
-    for (int i = 0; i < n; i++) {  /
+    for (int i = 0; i < n; i++) {  
         for (int j = 0; j < p; j++) { 
             matriz_C[i][j] = 0; 
             for (int k = 0; k < m; k++) {  
@@ -20,38 +20,75 @@ vector<vector<int> cubico_trad(vector<vector<int>> &matriz_A, vector<vector<int>
 
 
 int main(){
-    ifstream archivo("dataset.txt");
-    ofstream archivo_salida("salidas/salida_cubico_trad.txt");
-    ofstream archivo_salida2("salidas para graficar/salida_cubico_trad2.txt");
-    int casos, n, m, p;
-    archivo >> casos;
-    for (int i = 0; i < casos; i++) {
-        archivo >> n >> m;
-        vector<vector<int>> matriz_A(n, vector<int>(m));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                archivo >> matriz_A[i][j];
-            }
-        }
-        archivo >> m >> p;
-        vector<vector<int>> matriz_B(m, vector<int>(p));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < p; j++) {
-                archivo >> matriz_B[i][j];
-            }
-        }
-        auto start = chrono::high_resolution_clock::now();
-        vector<vector<int>> matriz_C = cubico_trad(matriz_A, matriz_B);
-        auto end = chrono::high_resolution_clock::now();
-        auto duracion = chrono::duration_cast<chrono::nanoseconds>(end - start);
+    ifstream nmp("datasets/dataset_mnp.txt");
+    ifstream nn("datasets/dataset_nxn.txt");
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < p; j++) {
-                archivo_salida << matriz_C[i][j] << " ";
+    ofstream sal_mnp("salidas/cubico_trad_salida_mnp.txt");
+    ofstream sal_nn("salidas/cubico_trad_salida_nn.txt");
+
+    ofstream gr_mnp("salidas para graficar/cubico_trad_salida_mnp.txt");
+    ofstream gr_nn("salidas para graficar/cubico_trad_salida_nn.txt");
+    int casos, n, m, p;
+    nmp >> casos;
+    for (int i = 0; i < casos; i++) {
+        nmp >> n >> m;
+        vector<vector<int>> matriz_A(n, vector<int>(m));
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < m; k++) {
+                nmp >> matriz_A[j][k];
             }
-            archivo_salida << "\n";
         }
-        archivo_salida << duracion.count() << "\n";
-        archivo_salida2 << n << "x" << m << " " << m << "x" << p << " " << duracion.count() << "\n";
+        nmp >> m >> p;
+        vector<vector<int>> matriz_B(m, vector<int>(p));
+        for (int j = 0; j < m; j++) {
+            for (int k = 0; k < p; k++) {
+                nmp >> matriz_B[j][k];
+            }
+        }
+        vector<vector<int>> matriz_C;
+        auto start = chrono::steady_clock::now();
+        matriz_C = cubico_trad(matriz_A, matriz_B);
+        auto end = chrono::steady_clock::now();
+        auto duracion = chrono::duration_cast<chrono::nanoseconds>(end - start);
+        sal_mnp << duracion.count() << endl;
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < p; k++) {
+                sal_mnp << matriz_C[j][k] << " ";
+            }
+            sal_mnp << endl;
+        }
+        int vol = n * m * p;
+        gr_mnp << vol << " " << duracion.count() << endl;  
+    }
+    nn >> casos;
+    for (int i = 0; i < casos; i++) {
+        nn >> n >> n;
+        vector<vector<int>> matriz_A(n, vector<int>(n));
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                nn >> matriz_A[j][k];
+            }
+        }
+        nn >> n >> n;
+        vector<vector<int>> matriz_B(n, vector<int>(n));
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                nn >> matriz_B[j][k];
+            }
+        }
+        vector<vector<int>> matriz_C;
+        auto start = chrono::steady_clock::now();
+        matriz_C = cubico_trad(matriz_A, matriz_B);
+        auto end = chrono::steady_clock::now();
+        auto duracion = chrono::duration_cast<chrono::nanoseconds>(end - start);
+        sal_nn << duracion.count() << endl;
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                sal_nn << matriz_C[j][k] << " ";
+            }
+            sal_nn << endl;
+        }
+        int vol = n * n * n;
+        gr_nn << vol << " " << duracion.count() << endl;  
     }
 }
--- matrices/generador.py
+++ matrices/generador.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 
-with open("dataset.txt", "w") as archivo:
+with open("datasets/dataset_mnp.txt", "w") as archivo:
     casos = 150
     temp = 0
     archivo.write(str(casos) + "\n")
@@ -10,8 +10,6 @@ with open("dataset.txt", "w") as archivo:
     while casos > 0:
         n = historial[-1]
         m = random.randint(1, 200)
-        if random.random() < 0.6: # 60% de probabilidad de ser nxn
-            m = n
         historial.append(m)
         casos -= 1
         archivo.write(str(n) + " " + str(m) + "\n")
@@ -19,3 +17,21 @@ with open("dataset.txt", "w") as archivo:
             for j in range(m):
                 archivo.write(str(random.randint(1, 1000)) + " ")
             archivo.write("\n")
+
+
+with open("datasets/dataset_nxn.txt", "w") as archivo:
+    casos = 75
+    temp = 0
+    archivo.write(str(casos) + "\n")
+    # genera matrices de nxn
+    while casos > 0:
+        n = random.randint(1, 200)
+        casos -= 1
+        cont = 0
+        while cont < 2:
+            archivo.write(str(n) + " " + str(n) + "\n")
+            for i in range(n):
+                for j in range(n):
+                    archivo.write(str(random.randint(1, 1000)) + " ")
+                archivo.write("\n")
+            cont += 1
